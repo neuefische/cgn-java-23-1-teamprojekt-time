224f2b15ba0967d3b6e1614bfb7f5dafa44aa38f
hamideh <hamideh_h61@yahoo.com>
2023-02-23 14:22:10 +0100

    add postNewTask and AddTask in frontend, getAllTasks with error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Task} from "./model/Task";
+import {NewTask, Task} from "./model/Task";
 import axios from "axios";
 import {Route, Routes} from "react-router-dom";
 import TasksGallery from "./component/TasksGallery";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+import AddTask from "./component/AddTask";
 
 function App() {
   const [tasks,setTasks]=useState<Task[]>([]);
 
   function loadAllTasks(){
-  axios.get("/api/tasks")
-      .then(response=>response.data)
+  axios.get("/api/tasks/")
+      .then(response=>response.data.map(task=>({id:task.id,title:task.title,dateTime:new Date(task.dateTime)})))
       .then(setTasks)
       .catch(console.error)
   }
+
+  function postNewTask(newTask:NewTask){
+     return axios.post("/api/tasks/",newTask)
+          .then(response=>{
+              setTasks(prevState => [...prevState,response.data])
+          })
+          .catch(console.error)
+  }
+
   useEffect(()=> {
       loadAllTasks()
   },[])
@@ -22,6 +34,7 @@ function App() {
     <div className="App">
        <Routes>
            <Route path={"/tasks"} element={<TasksGallery tasks={tasks}/>}/>
+           <Route path={"/addTask"} element={<AddTask onAdd={postNewTask}/>}/>
        </Routes>
     </div>
   );
