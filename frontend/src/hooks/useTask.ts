55671e60f2cba170546389cf3d8177c221e09b99
hamideh <hamideh_h61@yahoo.com>
2023-02-24 16:02:59 +0100

    fix postNewTask in oneTask

--- frontend/src/hooks/useTask.ts
+++ frontend/src/hooks/useTask.ts
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {NewTask, Task} from "../model/Task";
+import {Task} from "../model/Task";
 import axios from "axios";
 
 
@@ -21,13 +21,17 @@ function loadAllTasks(){
         .catch(console.error)
 }
 
-function postNewTask(newTask: NewTask){
-    return axios.post("/api/tasks/",newTask)
-        .then(response=>{
-            setTasks(prevState => [...prevState,response.data])
-        })
-        .catch(console.error)
-}
+    function postNewTask(newTask: Task){
+        return axios.post("/api/tasks/",newTask)
+            .then(response=>{
+                const returnedTask ={
+                    ...response.data,
+                    dateTime:new Date(response.data.dateTime)
+                }
+                setTasks(prevState => [...prevState,returnedTask])
+            })
+            .catch(console.error)
+    }
 
 useEffect(()=> {
     loadAllTasks()
