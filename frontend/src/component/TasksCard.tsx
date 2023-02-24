30296f1005a7ae920be6d2537564416e86a55dbe
valfink <117605562+valfink@users.noreply.github.com>
2023-02-24 10:07:31 +0100

    Add edit link

--- frontend/src/component/TasksCard.tsx
+++ frontend/src/component/TasksCard.tsx
@@ -1,6 +1,7 @@
 import {Task} from "../model/Task";
 import React from "react";
 import "./TaskCard.css";
+import {Link} from "react-router-dom";
 
 type Props={
     task:Task
@@ -11,7 +12,8 @@ export default function TasksCard(props:Props){
        <div className={"task-card"}>
            <h2>{props.task.title}</h2>
            {props.task.dateTime.getFullYear()} / {props.task.dateTime.getMonth()+1} / {props.task.dateTime.getDate()}<br/>
-           {props.task.dateTime.getHours()}:{props.task.dateTime.getMinutes()}
+           {props.task.dateTime.getHours()}:{props.task.dateTime.getMinutes()}<br />
+           <Link to={`/tasks/${props.task.id}/update`}>Edit</Link>
        </div>
    )
 }
