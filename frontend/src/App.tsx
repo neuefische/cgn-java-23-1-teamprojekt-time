81d7f3a5bc7b21eead899082caa7e7b7347a5aae
hamideh <hamideh_h61@yahoo.com>
2023-02-24 15:11:38 +0100

    add taskdetails in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {Route, Routes} from "react-router-dom";
 import TasksGallery from "./component/TasksGallery";
 import AddTask from "./component/AddTask";
 import useTask from "./hooks/useTask";
+import TaskDetails from "./component/TaskDetails";
 
 function App() {
     const {tasks,postNewTask}=useTask()
@@ -12,6 +13,7 @@ function App() {
        <Routes>
            <Route path={"/tasks"} element={<TasksGallery tasks={tasks}/>}/>
            <Route path={"/tasks/add"} element={<AddTask onAdd={postNewTask}/>}/>
+           <Route path={"/tasks/:id"} element={<TaskDetails tasks={tasks}/>}/>
        </Routes>
     </div>
   );
