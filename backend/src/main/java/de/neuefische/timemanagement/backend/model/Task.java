782f90f034b3b5193c540efd15c9baff1b505c9d
Edona Krosa Islami <MY_NAME@example.com>
2023-03-01 15:59:01 +0100

    remove empty lines

--- backend/src/main/java/de/neuefische/timemanagement/backend/model/Task.java
+++ backend/src/main/java/de/neuefische/timemanagement/backend/model/Task.java
@@ -1,11 +1,9 @@
 package de.neuefische.timemanagement.backend.model;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 
 @Document("tasks")
-
 public record Task(
         @Id
         String id,
