02f55ba4562abb43ac2343e59349a2ad5648d079
Edona Krosa Islami <MY_NAME@example.com>
2023-03-01 12:04:12 +0100

    Fix updateTask in hook

--- frontend/src/hooks/useTasks.ts
+++ frontend/src/hooks/useTasks.ts
@@ -36,7 +36,10 @@ function loadAllTasks(){
                 setTasks(prevState => {
                         return prevState.map(currentTask=>{
                             if (currentTask.id===task.id){
-                                return response.data
+                                return {
+                                    ...response.data,
+                                    dateTime:new Date(response.data.dateTime)
+                                }
                             }
                             return currentTask
                         })
