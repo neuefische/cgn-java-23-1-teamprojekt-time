2cf6730b6b6c75fe5ba8851abe840635df960d58
Edona Krosa Islami <MY_NAME@example.com>
2023-03-02 15:40:21 +0100

    temp commit

--- frontend/src/component/DayGallery.tsx
+++ frontend/src/component/DayGallery.tsx
@@ -1,5 +1,14 @@
+import {useParams} from "react-router-dom";
+
 export default function DayGallery() {
+    const params = useParams()
+    const year = params.year
+    const month = params.month
+    const day = params.da
+
+
     return (
-        <>GALLERY</>
+        <>
+        </>
     )
 }
